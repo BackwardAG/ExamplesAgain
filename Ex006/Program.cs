40c3b6485f490105883998589664e5ac962b6070
Armen G <ar.gukasov@gmail.com>
2023-01-12 19:23:20 +0300

    project 6 added

---
+++ Ex006/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name, ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "armen")
+{
+    Console.WriteLine("Great, it is Armen!");
+}
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);
+}
